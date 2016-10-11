c6a5e3689901aed5673b720c40e52b80ba8120b1
Paul Kane <kanepa1977@gmail.com>
2016-10-11 14:45:30 +0100

    albums changes

--- helloflask.py
+++ helloflask.py
@@ -3,10 +3,22 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+
+albums =[{
+    'artist': 'Madonna',
+    'title' : 'True Blue',
+    'year' : '1987'
+}]
+
+genres =['rock', 'blues', 'pop']
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/albums')
+def list_albums():
+    return render_template('helloflask.html', albums=albums, genres=genres)
 
 if __name__ == '__main__':
     app.run()
